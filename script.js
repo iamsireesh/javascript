7728edb5e743e72f1c8a550a84d02a90caf18923
sireesh <iamsireesh@gmail.com>
2018-04-22 13:16:44 +0530

    Data Types Intro

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hello World! Welcome to JavaScript Language Basics")
+
+//Data Types
+console.log("**********Data Types**********");
+var firstName = 'Sireesh';
+console.log(firstName);
+
+var lastName = "Vattikuti"
+console.log(lastName);
+
+var age = 28;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+
+var hello;
+console.log(hello);
